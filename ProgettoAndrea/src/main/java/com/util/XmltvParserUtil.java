74c9eceb4685b1e55fdbc7b0bfe86753a4f2cf83
Andrea Ficeti <andrea.ficeti@hp.com>
2016-06-09 17:27:05 +0200

    ànlkòùkèk

--- ProgettoAndrea/src/main/java/com/util/XmltvParserUtil.java
+++ ProgettoAndrea/src/main/java/com/util/XmltvParserUtil.java
@@ -1,6 +1,11 @@
 package com.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import javax.swing.text.Document;
@@ -20,16 +25,32 @@ public class XmltvParserUtil {
 	public static List<ProgrammaTvDTO> getProgrammiTV() {
 		// TODO Auto-generated method stub
 		try {
+			String link="http://www.mymovies.it/v9/tv/ajax/programmatv.asp"
+					+ "?var_tipoprogramma=%20((FILM.ID_TIPO_FILM)=%22FILM%22)%20OR%20"
+					+ "((FILM.ID_TIPO_FILM)=%22FLTV%22)%20%20&fascia=((PROGRAMMAZIONE_TV.ORA_FILM)%3E=2)"
+					+ "%20AND%20((PROGRAMMAZIONE_TV.ORA_FILM)%3C24)%20OR%20((PROGRAMMAZIONE_TV.ORA_FILM)%3E=24)"
+					+ "%20&var_id_canale=&var_tipo_canale=s&variabile3=&div=idprogramma";
+			
+			URL url = new URL(link);
+			HttpURLConnection uc = ProxyUtil.connect(url);
+			String line = null;
+			StringBuffer tmp = new StringBuffer();
+			BufferedReader in;
+			try{
+				in = new BufferedReader(new InputStreamReader(uc.getInputStream(),"UTF-8"));
 
-			File fXmlFile = new File(ProjectConfigUtil.getXmltvPath());
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			org.w3c.dom.Document doc = dBuilder.parse(fXmlFile);
-			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+			}
+			catch(FileNotFoundException ex)
+			{
+				throw ex;
+			}
+			while ((line = in.readLine()) != null) {
+				tmp.append(line);
+			}
+
 
 			//optional, but recommended
 			//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-			doc.getDocumentElement().normalize();
 		}
 		catch(Exception ex)
 		{
